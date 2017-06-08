633a383abd203e05e184e31f675d4d9b515cf390
Ryan Petschek <petschekr@gmail.com>
2017-06-08 17:30:38 -0400

    Publish detected objects to the TF tree

--- src/object_location/object_location_engine.py
+++ src/object_location/object_location_engine.py
@@ -1,5 +1,6 @@
 import rospy
 import sensor_msgs.point_cloud2 as pc2
+import tf
 from geometry_msgs.msg import Point, Pose, Quaternion
 from object_location.msg import Object
 from object_location.srv import LocationQuery, LocationQueryResponse
@@ -12,24 +13,40 @@ class ObjectLocationEngine():
 
     def __init__(self):
         self.object_detector_srv = rospy.get_param("~object_detector_service", "/detector_node/objects_in_scene")
+        self.global_frame = rospy.get_param("~global_frame", "kinect_rgb_optical_frame")
+        self.publish_to_tf_tree = rospy.get_param("~publish_as_tf", True)
+        
+        if self.publish_to_tf_tree:
+            rospy.loginfo("Will publish object locations to the TF tree as children of {}".format(self.global_frame))
         
         rospy.loginfo("Waiting for object detector service...")
         rospy.wait_for_service(self.object_detector_srv)
         rospy.loginfo("Object detector service loaded")
 
         # Incoming messages and services
+        self.objects = []
         self.object_detector = rospy.ServiceProxy(self.object_detector_srv, SceneQuery)
         
         self.frame = None
         rospy.Subscriber("/kinect/hd/points", PointCloud2, self.pointCloudUpdate, queue_size=1)
 
         # Outgoing messages and services
-        self.mainQueryService = rospy.Service("location_query", LocationQuery, self.handleLocationQuery)
-        
-        # For testing only
-        #self.handleLocationQuery(None)
+        self.main_query_service = rospy.Service("location_query", LocationQuery, self.handleLocationQuery)
 
-        rospy.spin()
+        if self.publish_to_tf_tree:
+            tfBroadcaster = tf.TransformBroadcaster()
+            rate = rospy.Rate(5)
+            while not rospy.is_shutdown():
+                for item in self.objects:
+                    position = (item.pose.position.x,
+                                item.pose.position.y,
+                                item.pose.position.z)
+                    rotation = (0, 0, 0, 1)
+
+                    tfBroadcaster.sendTransform(position, rotation, rospy.Time.now(), item.label, self.global_frame)
+                rate.sleep()
+        else:
+            rospy.spin()
     
     def handleLocationQuery(self, req):
         rospy.loginfo("Sending query...")
@@ -40,24 +57,28 @@ class ObjectLocationEngine():
             rospy.logwarn("No depth frame processed yet")
             return
 
-        objects = []
+        self.objects = []
+        tfBroadcaster = tf.TransformBroadcaster()
+
         for item in result.objects:
+            positionTuple = self.frame[item.centroid_y][item.centroid_x]
+
             remappedObject = Object()
             remappedObject.label = item.label
             remappedObject.probability = item.probability
 
             (remappedObject.pose.position.x,
              remappedObject.pose.position.y,
-             remappedObject.pose.position.z) = self.frame[item.centroid_y][item.centroid_x]
+             remappedObject.pose.position.z) = positionTuple
 
             print("Found \"{}\" with probability {} and centroid (x: {}, y: {})".format(item.label, item.probability, item.centroid_x, item.centroid_y))
             print("Full 3D position for this object is:\n{}".format(remappedObject.pose.position))
 
-            objects.append(remappedObject)
+            self.objects.append(remappedObject)
 
         result = LocationQueryResponse()
         result.header = Header()
-        result.objects = objects
+        result.objects = self.objects
         return result
 
     def pointCloudUpdate(self, data):
